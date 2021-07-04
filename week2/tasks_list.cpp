33d39c94f7076a600c7cb9c682a28a341ff84976
gleonell <aisaevr999@gmail.com>
2021-07-04 23:58:35 +0300

    function NEXT added

--- week2/tasks_list.cpp
+++ week2/tasks_list.cpp
@@ -21,14 +21,42 @@ void    call_dump(const vector<vector<string>>& list)
     cout << list[i].size() << " ";
     for (auto x: list[i])
         cout << x << " ";
+    cout << endl;
+}
+
+vector<vector<string>>    call_next(const vector<vector<string>>& list, bool& is_february, int& circle)
+{
+    vector<vector<string>> result;
+
+    result.insert(end(result), begin(list), end(list));
+    if (result.size() == 32)
+    {
+        if (is_february == 0){
+            for (int i = 29; i <= 31; i++)
+                result[28].insert(end(result[28]), begin(list[i]), end(list[i]));
+            result.resize(29);
+            is_february = 1;}
+        else if(circle == 12 || circle == 7){
+            if (circle == 12){
+                is_february = 0;
+                circle = 0;}
+            return result;}
+        else{
+            result[30].insert(end(result[30]), begin(list[31]), end(list[31]));
+            result.resize(31);}
+    }
+    else
+        result.resize(32);
+    return result;
 }
 
 int     main()
 {
-    vector<vector<string>> day_list(31);
+    vector<vector<string>> day_list(32);
     string cmd;
     int q;
-    //int first_in_flag = 0;
+    bool is_february = 0;
+    int circle = 0;
     
     cin >> q;
 
@@ -41,6 +69,11 @@ int     main()
             call_add(day_list);
         else if (cmd == "DUMP")
             call_dump(day_list);
+        else if (cmd == "NEXT")
+        {
+            circle++;
+            day_list = call_next(day_list, is_february, circle);
+        }
         else
             break;
         q--;
